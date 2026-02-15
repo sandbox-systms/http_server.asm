739b40b9af0289892b8dca5646e4be14ee76cbf1
sandbox <ghostnether28@gmail.com>
2026-02-15 12:56:58 -0300

    Estrutura local

---
+++ src/main.asm
@@ -0,0 +1,9 @@
+; ==========================================
+; Objetivo: Responder sempre a mesma pagina.
+; ========================================== 
+
+; ========================================= 
+; Fluxo: 
+; Criar socket AF_INET, SOCK_STREAM 
+; Bind socket a porta 80
+; 
